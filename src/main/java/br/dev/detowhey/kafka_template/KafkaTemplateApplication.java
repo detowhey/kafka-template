5d54de24d8ae369417ef7fbea77c262530b8ea81
henrique_almeida <henrique_almeida@sicredi.com.br>
2026-07-05 16:33:59 -0300

    feat: first commit for project

---
+++ src/main/java/br/dev/detowhey/kafka_template/KafkaTemplateApplication.java
@@ -0,0 +1,13 @@
+package br.dev.detowhey.kafka_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/detowhey/kafka_template/KafkaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.detowhey.kafka_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
